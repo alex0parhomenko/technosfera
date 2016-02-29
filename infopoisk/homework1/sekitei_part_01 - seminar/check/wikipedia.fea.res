e8b74ebc79a493c73a9bff70853476a91524b5d4
Alex <alex0parhomenko@gmail.com>
2016-02-29 21:50:17 +0300

    update feature extraction

---
+++ infopoisk/homework1/sekitei_part_01 - seminar/check/wikipedia.fea.res
@@ -0,0 +1,4 @@
+segment_name_0:wiki	1986
+segment_len_0:4	1986
+segments:2	1938
+segment_ext_1:jpg	150
--- infopoisk/homework1/sekitei_part_01 - seminar/extract_features.py
+++ infopoisk/homework1/sekitei_part_01 - seminar/extract_features.py
@@ -50,16 +50,17 @@ def extract_features(INPUT_FILE_1, INPUT_FILE_2, OUTPUT_FILE):
 	substr2 = re.compile(r'^[a-zA-Z]*[0-9]+[a-zA-Z]+$')
 
 	for i in range(len(result_url_list)):
+		segments.update({"segments:" + str(len(result_url_list[i])): segments.get("segments:" + str(len(result_url_list[i])), 0) + 1})
+
 		for j in range(len(result_url_list[i])):
 
-			segments.update({"segments:" + str(len(result_url_list[i])): segments.get("segments:" + str(len(result_url_list[i])), 0) + 1})
 
 			d = urlparse.parse_qs(urlparse.urlparse(result_url_list[i][j]).query)
 			for key in d:
 				param_name.update({"param_name:" + key: param_name.get("param_name:" + key, 0) + 1})
 				param.update({"param:" + key + "=" + str(d[key]) : param.get("param:" + key + "=" + str(d[key]), 0) + 1})
 
-			match_with_string.update({"segment_name_" + str(j) + ":" + str(result[i][j]): match_with_string.get("segment_name_" + str(j) + ":" + str(result[i][j]), 0) + 1})
+			match_with_string.update({"segment_name_" + str(j) + ":" + str(result_url_list[i][j]): match_with_string.get("segment_name_" + str(j) + ":" + str(result_url_list[i][j]), 0) + 1})
 
 			if (result_url_list[i][j].isdigit()):
 				consist_of_numbers.update({"segment_[0-9]_" + str(j) + ":1": consist_of_numbers.get("segment_[0-9]_" + str(j) + ":1", 0) + 1})
@@ -68,36 +69,38 @@ def extract_features(INPUT_FILE_1, INPUT_FILE_2, OUTPUT_FILE):
 				str_num_str.update({"segment_substr[0-9]_" + str(j) + ":1": str_num_str.get("segment_substr[0-9]_" + str(j) + ":1", 0) + 1})
 
 			s = result_url_list[i][j].rsplit('.', 1)
-			if (len(s[1]) != 0):
+			if (len(s) > 1 and len(s[1]) != 0):
 				if ((len(substr1.findall(s[0])) + len(substr2.findall(s[0])) > 0)):
 					expansion_and_str_num_str.update({"segment_ext_substr[0-9]_" + str(j) + ":" + s[1]: expansion_and_str_num_str.get("segment_ext_substr[0-9]_" + str(j) + ":" + s[1], 0) + 1})
-				expansion.update({"segment_ext_" + str(j) ":" + s[1]: expansion.get("segment_ext_" + str(j) ":" + s[1], 0) + 1})
+				expansion.update({"segment_ext_" + str(j) + ":" + s[1]: expansion.get("segment_ext_" + str(j) + ":" + s[1], 0) + 1})
 		
 			len_segments.update({"segment_len_" + str(j) +  ":" + str(len(result_url_list[i][j])): len_segments.get("segment_len_" + str(j) +  ":" + str(len(result_url_list[i][j])), 0) + 1})
 	result_features_list = []
 	for key in segments:
-		result_features_list.append(segments[key], key)
+		result_features_list.append([segments[key], key])
 	for key in len_segments:
-		result_features_list.append(len_segments[key], key)
+		result_features_list.append([len_segments[key], key])
 	for key in match_with_string:
-		result_features_list.append(match_with_string[key], key)
+		result_features_list.append([match_with_string[key], key])
 	for key in consist_of_numbers:
-		result_features_list.append(consist_of_numbers[key], key)
+		result_features_list.append([consist_of_numbers[key], key])
 	for key in str_num_str:
-		result_features_list.append(str_num_str[key], key)
+		result_features_list.append([str_num_str[key], key])
 	for key in expansion:
-		result_features_list.append(expansion[key], key)
+		result_features_list.append([expansion[key], key])
 	for key in expansion_and_str_num_str:
-		result_features_list.append(expansion_and_str_num_str[key], key)
+		result_features_list.append([expansion_and_str_num_str[key], key])
 	for key in param_name:
-		result_features_list.append(param_name[key], key)
+		result_features_list.append([param_name[key], key])
 	for key in param:
-		result_features_list.append(param[key], key)
-	sorted(result_features_list)
+		result_features_list.append([param[key], key])
+	result_features_list = sorted(result_features_list, reverse = True)
 	for i in range(len(result_features_list)):
-		f_out.write(result_features_list[i][1] + "\t" + result_features_list[i][0] + "\n")
+		if (result_features_list[i][0] <= 100):
+			break
+		f_out.write(str(result_features_list[i][1]) + "\t" + str(result_features_list[i][0]) + "\n")
 
 
 
-#extract_features("data/urls.lenta.examined", "data/urls.lenta.general", "output.txt")
+extract_features("data/urls.lenta.examined", "data/urls.lenta.general", "output.txt")
 
