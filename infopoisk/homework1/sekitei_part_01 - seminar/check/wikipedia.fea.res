a0ff38e2588530f54fbb6dbf851f43cf74c19ff3
Alex <alex0parhomenko@gmail.com>
2016-03-01 10:41:28 +0300

    updata infopoisk

--- infopoisk/homework1/sekitei_part_01 - seminar/check/wikipedia.fea.res
+++ infopoisk/homework1/sekitei_part_01 - seminar/check/wikipedia.fea.res
@@ -1,4 +1,4 @@
-segment_name_0:wiki	1986
-segment_len_0:4	1986
-segments:2	1938
-segment_ext_1:jpg	150
+segment_name_0:wiki	1989
+segment_len_0:4	1989
+segments:2	1939
+segment_ext_1:jpg	144
--- infopoisk/homework1/sekitei_part_01 - seminar/extract_features.py
+++ infopoisk/homework1/sekitei_part_01 - seminar/extract_features.py
@@ -1,5 +1,3 @@
-
-
 import sys
 import re
 import random
@@ -8,7 +6,6 @@ import urlparse
 import numpy as np
 
 def extract_features(INPUT_FILE_1, INPUT_FILE_2, OUTPUT_FILE):
-
 	f_in1 = open(INPUT_FILE_1, "r")
 	f_in2 = open(INPUT_FILE_2, "r")
 	f_out = open(OUTPUT_FILE, "w")
@@ -34,7 +31,6 @@ def extract_features(INPUT_FILE_1, INPUT_FILE_2, OUTPUT_FILE):
 	np.random.shuffle(url_list2)
 	result_url_list = np.concatenate((url_list1[0:1000], url_list2[0:1000]), axis = 0)
 
-		
 	segments = {}
 	len_segments = {}
 	match_with_string = {}
@@ -53,8 +49,6 @@ def extract_features(INPUT_FILE_1, INPUT_FILE_2, OUTPUT_FILE):
 		segments.update({"segments:" + str(len(result_url_list[i])): segments.get("segments:" + str(len(result_url_list[i])), 0) + 1})
 
 		for j in range(len(result_url_list[i])):
-
-
 			d = urlparse.parse_qs(urlparse.urlparse(result_url_list[i][j]).query)
 			for key in d:
 				param_name.update({"param_name:" + key: param_name.get("param_name:" + key, 0) + 1})
@@ -75,32 +69,19 @@ def extract_features(INPUT_FILE_1, INPUT_FILE_2, OUTPUT_FILE):
 				expansion.update({"segment_ext_" + str(j) + ":" + s[1]: expansion.get("segment_ext_" + str(j) + ":" + s[1], 0) + 1})
 		
 			len_segments.update({"segment_len_" + str(j) +  ":" + str(len(result_url_list[i][j])): len_segments.get("segment_len_" + str(j) +  ":" + str(len(result_url_list[i][j])), 0) + 1})
+
 	result_features_list = []
-	for key in segments:
-		result_features_list.append([segments[key], key])
-	for key in len_segments:
-		result_features_list.append([len_segments[key], key])
-	for key in match_with_string:
-		result_features_list.append([match_with_string[key], key])
-	for key in consist_of_numbers:
-		result_features_list.append([consist_of_numbers[key], key])
-	for key in str_num_str:
-		result_features_list.append([str_num_str[key], key])
-	for key in expansion:
-		result_features_list.append([expansion[key], key])
-	for key in expansion_and_str_num_str:
-		result_features_list.append([expansion_and_str_num_str[key], key])
-	for key in param_name:
-		result_features_list.append([param_name[key], key])
-	for key in param:
-		result_features_list.append([param[key], key])
+	result_dicts = [segments, len_segments, match_with_string, consist_of_numbers, str_num_str, expansion, expansion_and_str_num_str, param_name, param]
+	for i in range(len(result_dicts)):
+		for key in result_dicts[i]:
+			result_features_list.append([result_dicts[i][key], key])
+
 	result_features_list = sorted(result_features_list, reverse = True)
-	for i in range(len(result_features_list)):
-		if (result_features_list[i][0] <= 100):
-			break
-		f_out.write(str(result_features_list[i][1]) + "\t" + str(result_features_list[i][0]) + "\n")
+	result_list = filter(lambda x: x[0] > 100, result_features_list)
+	for i in range(len(result_list)):
+		f_out.write(str(result_list[i][1]) + "\t" + str(result_list[i][0]) + "\n")
 
 
 
-extract_features("data/urls.lenta.examined", "data/urls.lenta.general", "output.txt")
+#extract_features("data/urls.lenta.examined", "data/urls.lenta.general", "output.txt")
 
