f7ab13dd315b78d61f885341b494ca0985376b13
Alexandr Shulgin <alex0parhomenko@gmail.com>
2016-03-19 21:22:35 +0300

    info

--- infopoisk/hw2/src/solution.py
+++ infopoisk/hw2/src/solution.py
@@ -7,7 +7,6 @@ import mmh3
 from collections import Counter
 from time import clock
 
-
 files = sys.argv
 del files[0]
 docs, urls = docreader.main(files)
@@ -16,7 +15,7 @@ hash_arr = []
 hash_count = 20
 shingle_k = 5
 shingle = ""
-
+hash_doc = Counter()
 for doc_id, doc in enumerate(docs):
 	now_hash = [np.inf] * hash_count 
 	words = doc.split(' ')
@@ -33,38 +32,29 @@ for doc_id, doc in enumerate(docs):
 
 hash_arr = sorted(hash_arr)
 
-pairs = []
 now_pos = []
+sim_arr = np.zeros((len(docs), len(docs)), dtype = np.float)
+
 for i in range(1, len(hash_arr)):
 	if (hash_arr[i][0] == hash_arr[i - 1][0]):
 		for p in now_pos:
-			pairs.append([min(hash_arr[i][1], hash_arr[p][1]), max(hash_arr[i][1], hash_arr[p][1])])
+			sim_arr[hash_arr[i][1]][hash_arr[p][1]] += 1
+			sim_arr[hash_arr[p][1]][hash_arr[i][1]] += 1
 		if (not now_pos):
 			now_pos.append(i - 1)
 			now_pos.append(i)
-			pairs.append([min(hash_arr[i][1], hash_arr[i - 1][1]), max(hash_arr[i][1], hash_arr[i - 1][1])])
+			sim_arr[hash_arr[i][1]][hash_arr[i - 1][1]] += 1
+			sim_arr[hash_arr[i - 1][1]][hash_arr[i][1]] += 1
 		else:
 			now_pos.append(i)
 	else:
 		now_pos = []
 
-pairs = sorted(pairs)
-
-result_pairs = Counter()
+hash_arr = []
 threshhold = 0.75
-for pair_num, pair in enumerate(pairs):
-	result_pairs[str(pair[0]) + " " + str(pair[1])] += 1
 
-
-sim_id = []
-for key in result_pairs:
-	if (result_pairs[key] * 1.0 / hash_count >= threshhold):
-		sim_id.append([result_pairs[key] * 1.0 / hash_count, key])
-sim_id = sorted(sim_id, reverse = True)
-
-for p in sim_id:
-	first, second = p[1].split(' ')
-  	print urls[int(first)] + " " + urls[int(second)] + " " + str(int(p[0] * 100))
-
-
+for doc_f in range(len(urls)):
+	for doc_s in range(doc_f + 1, len(urls)):
+		if (sim_arr[doc_f][doc_s] >= 15.0):
+			print urls[doc_f] + " " + urls[doc_s] + " " + str(sim_arr[doc_f][doc_s] * 5)
 
