e4d097e5c1850cd245f0957c5499717bf8b981af
Alexandr Shulgin <alex0parhomenko@gmail.com>
2016-04-01 16:23:40 +0300

    add reverse index

---
+++ C++/p4/check.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <limits.h>
+#include <time.h>
+
+using namespace std;
+
+#define f first
+#define s second
+#define mp make_pair
+#define file_name "in.bin"
+
+long long get_rand(long long low_bound, long long up_bound)
+{
+	double x = ((double)rand() / (double)RAND_MAX) * ((double)up_bound - (double)low_bound) + low_bound;
+	return (long long) x;
+}
+
+void print_example(long long doc1, vector <long long> v)
+{
+	cout << doc1 << " " << v.size() << " ";
+	for (auto i = 0; i < v.size(); i++)
+	{
+		cout << v[i];
+		if (i != v.size() - 1)
+			cout << " ";
+	}
+	cout << "\n";
+}
+
+int main(int argc, char * argv[])
+{
+	srand(time(NULL));
+	auto cou_examples = 10;
+	auto fd_out = open(file_name, O_RDONLY | O_WRONLY);
+	vector <pair <long long, vector <long long> > > index;
+	for (auto i = 0; i < cou_examples; i++)
+	{
+		vector <long long> v1;
+		long long doc1 = get_rand(1, 50);
+		int cou = (int)get_rand(1, 30);
+		for (auto j = 0; j < cou; j++)
+			v1.push_back(get_rand(1, 20));
+
+		write(fd_out, &doc1, sizeof(doc1));
+		write(fd_out, &cou, sizeof(cou));
+		for (auto j = 0; j < v1.size(); j++)
+		{
+			write(fd_out, &(v1[j]), sizeof(v1[j]));
+		}
+		print_example(doc1, v1);
+	}
+	close(fd_out);
+	return 0;
+}
--- C++/p4/sort_file.cpp
+++ C++/p4/sort_file.cpp
@@ -13,94 +13,217 @@
 #include <fcntl.h>
 #include <sstream>
 #include <fstream>
+#include <limits.h>
 
 #define byte8 8
 #define byte4 4
 #define simple_const 77
-#define make_pair mp
-#define first f
-#define second s
+#define mp make_pair
+#define f first
+#define s second
+#define mem_size 512
+#define file_in_name "in.bin"
+#define file_out_name "out.bin"
+#define result_file "result.bin"
 
-auto file_size = 0;
-auto memory_size = 0;
-auto max_use_memory = 0;
-auto now_file_num = 0;
+using namespace std;
+
+class offset{
+public:
+	long long now_pos;
+	long long end_pos;
+	offset(long long now_pos, long long end_pos) : now_pos(now_pos), end_pos(end_pos) {}
+};
+
+vector <offset> offsets_in_file;
+long long now_offset_out_file = 0;
+
+void print_map_in_file(map<long long, vector <long long> > m, int fd)
+{
+	auto beg = m.begin();
+	auto end = m.end();
+	auto note_size = 0;
+	while (beg != end)
+	{
+
+		long long word = beg->f;
+		//cout << "word is: " << word << " docs is: "; 
+		vector <long long> docs = beg->s;
+		auto it = unique(docs.begin(), docs.end());
+		docs.resize(distance(docs.begin(), it));
+		sort(docs.begin(), docs.end());
+		/*for (auto i : docs)
+		{
+			cout << i << " ";
+		}*/
+		int sz = docs.size();
+		write(fd, &word, sizeof(word));
+		write(fd, &sz, sizeof(sz));
+
+		note_size += sizeof(word);
+		note_size += sizeof(sz);
+
+		for (auto i = 0; i < docs.size(); i++)
+		{
+			write(fd, &(docs[i]), sizeof(docs[i]));
+			note_size += sizeof(docs[i]);
+		}
+		beg++;
+		//cout << endl;
+	}
+	//cout << endl;
+	offsets_in_file.push_back(offset(now_offset_out_file, now_offset_out_file + note_size));
+	now_offset_out_file += note_size;
+
+}
 
 int main(int argc, char * argv[])
 {
-	std::string f_s(argv[1]), m_s(argv[2]);
-	std::stringstream f_size(f_s), mem_s(m_s);
-	std::stringstream ss;
-	max_use_memory = memory_size / 2;
-	std::vector <std::string> file_names;
-	char buf[max_use_memory];
-	char buf_8[byte8 + 1];
-	char buf_4[byte4 + 1];
-	f_size >> file_size;
-	mem_s >> memory_size;
-	std::map <int, vector <int> > d;
-	std::set<int> words;
-	auto fd = open(argv[3]);
-	auto num_it = 0;
-	auto now_bytes = 0;
-	auto now_pos_in_file = 0;
-	auto cou = 0;
-	auto fd_now = 0;
-	auto now_pos_in_str;
-	auto is_eof = false;
+	auto fd_in = open(file_in_name, O_RDONLY);
+	auto fd_out = open(file_out_name, O_WRONLY | O_RDONLY);
+	auto fd_final = open(result_file, O_WRONLY);
+	auto BUF_SIZE = mem_size / 2;
+	char * buf = (char *)malloc(BUF_SIZE);
+	
+	long long doc_id = 0;
+	int word_count = 0;
+	long long now_word = 0;
+
+	long long old_doc_id = 0;
+	long long old_words_balance = 0;
+
+	map <long long, vector <long long> > doc_words;
+
+	auto start_pos = 0;
+	auto count = 0;
+	auto balance = BUF_SIZE - 1 - start_pos;
+	auto now_read = start_pos;
+	auto now_pos_in_str = 0;
+	auto now_iter = 0;
+	//cout << BUF_SIZE << endl;
 	while (true)
 	{
-		auto cou_blocks = 0;
-		while (true)
+		count = 0;
+		balance = BUF_SIZE - 1 - start_pos;
+		now_read = start_pos;
+		while (balance > 0 && (count = read(fd_in, buf + now_read, balance)) > 0)
 		{
-			cou = read(fd, buf_8, byte8);
-			if (cou <= 0)
-			{
-				is_eof = true;
-				break;
-			}
-			cou = read(fd, buf_4, byte4);
-			auto file_count = 0;
-			sscanf(buf_4, "%d", file_count);
-			fseek(fd, -12, SEEK_CUR);
-			if (now_bytes + byte8 + byte4 + file_count * byte8 + simple_const > max_use_memory)
-				break;	
-			else
-			{
-				cou = read(fd, buf + now_bytes, byte8 + byte4 + file_count * 8);
-				now_bytes += byte8 + byte4 + file_count * 8;
-			}
-			cou_blocks++;
+			balance -= count;
+			now_read += count;
 		}
-		buf[now_bytes] = '\0';
-		std::vector <pair<int, int>> word_doc;
-		auto now_pos_in_str = 0;
-		for (auto i = 0; i < cou_blocks; i++)
-		{
-			auto doc_id = 0;
-			auto cou_f = 0;
-			sscanf(buf, "%d", &doc_id);
-			sscanf(buf + 4, "%d", &cou_f);
-			now_pos_in_str += 8;
-			for (auto j = 0; j < cou_f; j++)
-			{
-				auto word = 0;
-				sscanf(buf, "%d", &word);
-				word_doc.push_back(mp(word, doc_id));
-			}
-		}
-		for (auto p in word_doc)
-		{
-			d[p[0]].push_back(p[1]);
-			words.insert(word);
-		}
-		memset(buf, 0, sizeof(buf));
-		ss << now_file_num;
-		file_names.push_back(ss.str());
-		fd_now = open(file_names[file_names.size() - 1], O_CREAT|O_WRONLY);
-		now_file_num++;
-	}
+		//cout << "new read" << endl;
+		now_pos_in_str = 0;
+		balance = now_read;
 
-	close(fd);
+		if (old_words_balance > 0)
+		{
+			for (auto i = 0; i < old_words_balance; i++)
+			{
+				memcpy(&now_word, buf + now_pos_in_str, byte8);
+				//cout << "now_write_word_with_add: " << now_word << endl;
+				now_pos_in_str += byte8;
+				balance -= byte8;
+				doc_words[now_word].push_back(old_doc_id);
+				//cout << old_doc_id << endl;
+			}
+			old_words_balance = 0;
+		}
+		if (!doc_words.empty())
+		{
+			//cout << "all good" << endl;
+			print_map_in_file(doc_words, fd_out);
+			doc_words.clear();
+		}
+		while (balance > 12)
+		{
+			//if (balance <= 12)
+			//	break;
+
+			memcpy(&doc_id, buf + now_pos_in_str, byte8);
+			now_pos_in_str += byte8;
+			memcpy(&word_count, buf + now_pos_in_str, byte4);
+			now_pos_in_str += byte4;
+			//cout << "doc_id count_words: " << doc_id << " " << word_count << endl;
+
+			balance -= byte8 + byte4;
+
+			old_words_balance = word_count;
+			old_doc_id = doc_id;
+
+			for (auto i = 0; i < word_count && balance >= 8; i++)
+			{
+				now_word = 0;
+				memcpy(&now_word, buf + now_pos_in_str, byte8);
+				//cout << "now_write_word: " << now_word << endl;
+				if (doc_words.count(now_word))
+				{
+					doc_words[now_word].push_back(doc_id);
+					//cout << doc_id << endl;
+				}
+				else
+				{
+					vector <long long> v{doc_id};
+					//cout << doc_id << endl;
+					doc_words.insert(doc_words.begin(), pair <int, vector<long long> >(now_word, v));
+				}
+				now_pos_in_str += byte8;
+				balance -= byte8;
+				old_words_balance--;
+			}
+			//cout << "balance is: " << balance << endl;
+		}
+		if (balance > 0)
+		{
+			memcpy(buf, buf + now_pos_in_str, balance);
+			start_pos = balance;
+		}
+		if (count <= 0)
+			break;
+		now_iter++;
+	}
+	if (!doc_words.empty())
+	{
+		//cout << "all good" << endl;
+		print_map_in_file(doc_words, fd_out);
+		doc_words.clear();
+	}
+	//cout << "all good this is end" << endl;
+	//return 0;
+	for (auto x : offsets_in_file)
+	{
+		cout << x.now_pos << " " << x.end_pos << endl;
+	}
+	close(fd_in);
+
+	vector <long long> words;
+	long long min_word = LLONG_MAX;
+	for (auto x : offsets_in_file)
+	{
+		long long word = 0;
+		lseek(fd_final, x.now_pos, SEEK_SET);
+		read(fd_final, &word, byte8);
+		words.push_back(word);
+		min_word = min(min_word, word);
+	}
+	while (true)
+	{
+
+		bool flag = 0;
+		for (auto x : offsets_in_file)
+		{
+			if (x.now_pos == x.end_pos)
+				continue;
+			else
+				flag = 1;
+			long long word = 0;
+			lseek(fd_final, x.now_pos, SEEK_SET);
+			read(fd_final, &word, byte8);
+			words.push_back(word);
+			min_word = min(min_word, word);
+		}
+		if (!flag)
+			break;
+	}
+	close(fd_out);
 	return 0;
 }
